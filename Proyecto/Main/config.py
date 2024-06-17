a0c09eeb4732b7f135f63b46c5aa136c9532ae8d
Ezekivel <123662355+Ezekivel@users.noreply.github.com>
2024-06-17 14:24:23 -0600

    Update config.py

--- Proyecto/Main/config.py
+++ Proyecto/Main/config.py
@@ -1,3 +1,4 @@
-api_key = "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"
-custom_search = "AIzaSyChBXnG90nsXFMwx9PKLHLWnS2KxDwJ2FQ"
-custom_search_cx = "33f5a4b37f3a6414a"
+#Aquí deben de ir las claves del motor de búsqueda.
+api_key = ""
+custom_search = ""
+custom_search_cx = ""
